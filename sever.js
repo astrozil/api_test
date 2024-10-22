a715ea3c8176df85ac38ed7456cfa01e33438be1
Astro <87592586+astrozil@users.noreply.github.com>
2024-10-22 23:23:56 +0630

    t

--- sever.js
+++ sever.js
@@ -22,7 +22,7 @@ app.get('/convert', async (req, res) => {
 
         return res.json({ mp3Url: audioUrl });
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to fetch video information.' });
+        return res.status(500).json({ error: error});
     }
 });
 
